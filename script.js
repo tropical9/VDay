a6e544d584d4f0a65dd6108cd24d04fc199c7244
Sharmaine <113566952+tropical9@users.noreply.github.com>
2025-02-14 20:21:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,14 @@ const gifs = [
   "https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif", // Bank loan
   "https://media.giphy.com/media/l0MYEqEzwMWFCg8rm/giphy.gif", // Camera
   "https://media.giphy.com/media/3o7btPCcdNniyf0ArS/giphy.gif", // Time traveler
-  
+  "https://media3.giphy.com/media/8m4R4pvViWtRzbloJ1/giphy.gif?cid=6c09b952tdwmulql1k571m481vn65id8gw7fa8h87f9i0c71&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g",
+  "https://media1.giphy.com/media/l4Jz3a8jO92crUlWM/giphy.gif?cid=6c09b952vmqrr5zy81rk50fkjdo6oigjub5kb36t2udjgor6&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g",
+"https://media0.giphy.com/media/fXJyMfUdqVCMPAnPJM/giphy.gif?cid=6c09b952yf0e5z873wlrjpp1y31w9l7e4iw7m62vo7tzb2j2&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g",
+"https://media1.giphy.com/media/Z5xk7fGO5FjjTElnpT/giphy.gif?cid=6c09b952rstn1yvjzpsxw4asp2iqg01c2m5is4zm9dc6tu3n&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g",
+"https://media0.giphy.com/media/8g63zqQ5RPt60/giphy.gif?cid=6c09b952u1rosuemv2yqirn6uba30cjg1pffdxi24r45v88z&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g",
+"https://media2.giphy.com/media/Lopx9eUi34rbq/giphy.gif?cid=6c09b952sv5xnuewoky84vuubsiu6pbzhr4vtprv6i8a0igb&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g",
+
+"https://media0.giphy.com/media/26xBPncFx3h3MQd8s/giphy.gif?cid=6c09b9529yv6leppn1pvnl6r9vd5ulbdqw1oy07hoa9df5gz&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g",
 ];
 
 const jokeElement = document.getElementById("joke");
